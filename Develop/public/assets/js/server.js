9b739a1472ff4a5ac00bd5c5d45de9d438ba282d
Wendi Freeman <wfreeman2112@gmail.com>
2020-12-12 21:03:24 -0600

    API POST route added

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -31,6 +31,11 @@ app.get("/api/notes/:note", function(req, res) {
     return res.json 
 });
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+}
 
 //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
